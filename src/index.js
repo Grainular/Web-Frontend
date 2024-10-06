f49d94602757dae2ff405795539c9ba8ac1c80ee
Paras Nath Seth <parasnathseth5@gmail.com>
2024-10-05 22:36:20 -0300

    Moved 'index.css' into the 'css' folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';  // Updated import statement
-import './index.css';
+import './css/index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import Homepage from './components/Homepage';
